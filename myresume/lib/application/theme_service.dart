1983e0637134fd8207bd783b547932543f8c5f37
sergo <gengel.ss@gmail.com>
2024-04-17 17:59:13 +0300

    fin

--- myresume/lib/application/theme_service.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeService extends ChangeNotifier {
-  bool isDarkModeOn = true;
-  void toggleTheme() {
-    this.isDarkModeOn = !this.isDarkModeOn;
-    notifyListeners();
-  }
-}
